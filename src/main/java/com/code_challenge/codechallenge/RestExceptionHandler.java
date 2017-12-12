694cb1c9384118b99a5b0a3b1820b8fb965b0851
lu92 <biklucjan@gmail.com>
2017-12-12 01:01:24 +0100

    delete unnecessary EntityNotFoundException

--- src/main/java/com/code_challenge/codechallenge/RestExceptionHandler.java
+++ src/main/java/com/code_challenge/codechallenge/RestExceptionHandler.java
@@ -105,21 +105,6 @@ public class RestExceptionHandler extends ResponseEntityExceptionHandler {
         return buildResponseEntity(apiError);
     }
 
-    /**
-     * Handles EntityNotFoundException. Created to encapsulate errors with more detail than javax.persistence.EntityNotFoundException.
-     *
-     * @param ex the EntityNotFoundException
-     * @return the ApiError object
-     */
-    @ExceptionHandler(EntityNotFoundException.class)
-    protected ResponseEntity<Object> handleEntityNotFound(
-            EntityNotFoundException ex) {
-        ApiError apiError = new ApiError(NOT_FOUND);
-        apiError.setMessage(ex.getMessage());
-        return buildResponseEntity(apiError);
-    }
-
-
     @ExceptionHandler(UserAlreadyExistsException.class)
     protected ResponseEntity<Object> handleUserAlreadyExist(
             UserAlreadyExistsException ex) {
@@ -183,45 +168,6 @@ public class RestExceptionHandler extends ResponseEntityExceptionHandler {
         String error = "Error writing JSON output";
         return buildResponseEntity(new ApiError(HttpStatus.INTERNAL_SERVER_ERROR, error, ex));
     }
-
-//    /**
-//     * Handle javax.persistence.EntityNotFoundException
-//     */
-//    @ExceptionHandler(javax.persistence.EntityNotFoundException.class)
-//    protected ResponseEntity<Object> handleEntityNotFound(javax.persistence.EntityNotFoundException ex) {
-//        return buildResponseEntity(new ApiError(HttpStatus.NOT_FOUND, ex));
-//    }
-//
-//    /**
-//     * Handle DataIntegrityViolationException, inspects the cause for different DB causes.
-//     *
-//     * @param ex the DataIntegrityViolationException
-//     * @return the ApiError object
-//     */
-//    @ExceptionHandler(DataIntegrityViolationException.class)
-//    protected ResponseEntity<Object> handleDataIntegrityViolation(DataIntegrityViolationException ex,
-//                                                                  WebRequest request) {
-//        if (ex.getCause() instanceof ConstraintViolationException) {
-//            return buildResponseEntity(new ApiError(HttpStatus.CONFLICT, "Database error", ex.getCause()));
-//        }
-//        return buildResponseEntity(new ApiError(HttpStatus.INTERNAL_SERVER_ERROR, ex));
-//    }
-
-    /**
-     * Handle Exception, handle generic Exception.class
-     *
-     * @param ex the Exception
-     * @return the ApiError object
-     */
-    @ExceptionHandler(MethodArgumentTypeMismatchException.class)
-    protected ResponseEntity<Object> handleMethodArgumentTypeMismatch(MethodArgumentTypeMismatchException ex,
-                                                                      WebRequest request) {
-        ApiError apiError = new ApiError(BAD_REQUEST);
-        apiError.setMessage(String.format("The parameter '%s' of value '%s' could not be converted to type '%s'", ex.getName(), ex.getValue(), ex.getRequiredType().getSimpleName()));
-        apiError.setDebugMessage(ex.getMessage());
-        return buildResponseEntity(apiError);
-    }
-
 
     private ResponseEntity<Object> buildResponseEntity(ApiError apiError) {
         return new ResponseEntity<>(apiError, apiError.getStatus());
